fc5f4ef058c2740b764933805f8631e88dfbaac6
brendon-boldt <brendon_boldt@yahoo.com>
2015-01-25 15:43:05 -0500

    Family member referencing now done via member methods

--- node.py
+++ node.py
@@ -1,17 +1,17 @@
 class Node:
   color = 'Red'
   value = None
+  left = None
+  right = None
   parent = None
-  grandparent = None
-  uncle = None
-  brother = None
+  side = None
 
   def __init__(self, value):
     self.value = value
 
   def initChildren(self):
-    self.child0 = Node(None)
-    self.child1 = Node(None)
+    self.left = Node(None)
+    self.right = Node(None)
   
   @staticmethod
   def initTree(value):
@@ -21,12 +21,6 @@ class Node:
     case1(node)
     return node
 
-  def assignFamily(self,parent,grandparent,uncle,brother):
-    self.parent = parent
-    self.grandparent = grandparent
-    self.uncle = uncle
-    self.brother = brother
-
   def __str__(self):
     if self.value == None:
       return 'nilNode'
@@ -34,43 +28,65 @@ class Node:
 
   def tree(self,level = 0):
     print '\t'*level, self
-    if self.child0.value != None:
-      self.child0.tree(level+1)
-    if self.child1.value != None:
-      self.child1.tree(level+1)
+    if self.left.value != None:
+      self.left.tree(level+1)
+    if self.right.value != None:
+      self.right.tree(level+1)
+
+  # Accessing family members means that only the parent needs to be updated
+  def grandparent(self):
+    return self.parent.parent
+
+  def uncle(self):
+    if(self.parent.side == 'Left'):
+      return self.grandparent().right
+    else:
+      return self.grandparent().left
+
+  def brother(self):
+    if(self.side == 'Left'):
+      return self.parent.right
+    else:
+      return self.parent.left
 
   def family(self):
-    print 'grandparent\t' + str(self.grandparent)
+    print 'left\t\t' + str(self.left)
+    print 'right\t\t' + str(self.right)
+    print 'grandparent\t' + str(self.grandparent())
     print 'parent\t\t' + str(self.parent)
-    print 'uncle\t\t' + str(self.uncle)
-    print 'brother\t\t' + str(self.brother)
+    print 'uncle\t\t' + str(self.uncle())
+    print 'brother\t\t' + str(self.brother())
 
   def insert(self,value):
-    if self.child0.value == None:
-      self.child0.initChildren()
-      self.child0.assignFamily(self,self.parent,self.brother,self.child1)
-      self.child0.value = value
-      case1(self.child0)
-    elif self.child1.value == None:
-      self.child1.initChildren()
-      self.child1.assignFamily(self,self.parent,self.brother,self.child0)
-      self.child1.value = value
-      case1(self.child1)
-    elif abs(self.child0.value-value) < abs(self.child1.value-value):
-      self.child0.insert(value)
+    if self.left.value == None:
+      self.left.initChildren()
+      self.left.parent = self
+      self.left.value = value
+      self.left.side = 'Left'
+      case1(self.left)
+    elif self.right.value == None:
+      self.right.initChildren()
+      self.right.parent = self
+      self.right.value = value
+      self.right.side = 'Right'
+      case1(self.right)
+    elif abs(self.left.value-value) < abs(self.right.value-value):
+      self.left.insert(value)
     else:
-      self.child1.insert(value)
+      self.right.insert(value)
     self.blr()
     #self.balance()
 
   # Balances the left and right side of a node (smaller value on right)
   def blr(self):
-    if self.child0.value == None:
-      self.child0 = self.child1
-    if self.child0.value == None or self.child1.value == None:
+    if self.left.value == None:
+      self.left = self.right
+    if self.left.value == None or self.right.value == None:
       return
-    if self.child0.value > self.child1.value:
-      self.child0, self.child1 = self.sN[1], self.sN[0] 
+    if self.left.value > self.right.value:
+      self.left, self.right = self.right, self.left
+    self.left.side = 'Left'
+    self.right.side = 'Right'
 
   def weight(self):
     count = 0
@@ -78,31 +94,34 @@ class Node:
       count += 1
     else:
       count += 0
-    if self.child0.value == None and self.child1.value == None:
+    if self.left.value == None and self.right.value == None:
       return count
-    if self.child1.value == None:
-      count += self.child0.weight()
+    if self.right.value == None:
+      count += self.left.weight()
     else:
-      count += self.child0.weight() + self.child1.weight()
+      count += self.left.weight() + self.right.weight()
     return count
  
   # Returns -1 if balanced; 0/1 if that node is heavier (more black subnodes) 
   def unbalanced(self):
-    if self.child0.value == None and self.child1.value == None:
+    if self.left.value == None and self.right.value == None:
       return -1
-    weight0 = self.child0.weight()
-    if self.child1.value == None:
+    weight0 = self.left.weight()
+    if self.right.value == None:
       if weight0 != 0:
         return 0
       else:
         return -1
-    weight1 = self.child1.weight()
+    weight1 = self.right.weight()
     if weight0 > weight1:
       return 0
     elif weight0 < weight1:
       return 1
     else:
       return -1
+    
+  def rotateLeft(self):
+    print None
 
 def case1(node):
   print node
@@ -121,17 +140,19 @@ def case2(node):
 
 def case3(node):
   print 'case3'
-  if node.parent.color == 'Red' and node.uncle.color == 'Red':
+  if node.parent.color == 'Red' and node.uncle().color == 'Red':
     node.parent.color = 'Black'
-    node.uncle.color = 'Black'
-    node.grandparent.color = 'Red'
-    case1(node.grandparent)
+    node.uncle().color = 'Black'
+    node.grandparent().color = 'Red'
+    case1(node.grandparent())
   else:
     case4(node)
 
 def case4(node):
   print 'case4'
-  print None
+  if node.side != node.parent.side:
+    print 'true'
+    
 
 def case5(node):
   print 'case5'
